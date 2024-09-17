01fa85d39a8d9e7cb7c81856cacc0db7dac107ab
GustavoFreiree <gustavostallmann@gmail.com>
2024-09-17 19:09:33 -0300

    update

--- lista/ex1.c
+++ lista/ex1.c
@@ -2,53 +2,54 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef int KEY; 
-
-typedef struct node_element { 
-    KEY data;
-    struct node_element *next; 
-} Node; 
-
 void alloc_error( ) {
     printf("Erro: memoria insuficiente");
     exit(1);
 }
 
-Node *init_list( ) {
-    Node *list = (Node *) malloc(sizeof(Node));
-    if (!list) alloc_error();
-    list->next = NULL; 
+typedef int key; 
+typedef struct nd {
+    key info; 
+    struct nd *next;
+} Node;
 
+typedef struct {
+    Node *first_node; 
+} List; 
+
+List* lst_init( ) {
+    List *list = (List *) malloc(sizeof(List));
+    list->first_node = NULL; 
     return list; 
-}
+};
 
-Node *get_last_node(Node *first_node) {
-    if (first_node->next == NULL) return first_node; 
-
-    return get_last_node(first_node->next);
-}
-
-bool push_node(Node *first_node, KEY data) {
-    Node *last_node = get_last_node(first_node);
-
+Node* lst_insert(key info, List *list) {
     Node *new_node = (Node *) malloc(sizeof(Node));
     if (!new_node) alloc_error();
 
-    last_node->next = new_node;
-    return true; 
+    new_node->info = info; 
+    new_node->next = list->first_node; 
+
+    list->first_node = new_node;
+
+    return new_node; 
 }
 
-void print_nodes(Node *first_node) {
-    if (!first_node->next) return; 
+void lst_print(List *list) {
+    Node *p; 
 
-    printf("%d", first_node->data);
-    return print_nodes(first_node->next);
+    for (p=list->first_node; p != NULL; p = p->next)
+        printf("[%d]", p->info);
 }
 
 int main( ) {
-    Node *list = init_list(); 
-    get_last_node(list);
-    // push_node(list, 10);
-    // print_nodes(list);
+    List *list = lst_init( ); 
+    lst_insert(10, list); 
+    lst_insert(20, list);
+    lst_print(list);
+
+    for (int i, j; i<10 && j<10; i++, j++)
+        printf("%d | %d\n", i, j);
+
     return 0;
 }
